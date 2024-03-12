0e74881080c1cbb0f7b4d2e8e2e040ec8a3d770d
João <71526834+txjao@users.noreply.github.com>
2024-03-12 02:06:29 -0300

    fix: improving the performance

--- two sums/two-sum.ts
+++ two sums/two-sum.ts
@@ -2,7 +2,6 @@ function twoSum(nums: number[], target: number): number[] {
     let indexes = [];
 
     for (let i = 0; i < nums.length; i++) {
-        const value = nums[i];
         for (let j = i + 1; j < nums.length; j++) {
             if (value + nums[j] === target) {
                 indexes.push(i, j);
